1c82e2a79e6e3fa4cc82ef3257cf018a732872b0
the_p <the_p@LAPTOP-77GCKEU2>
2022-05-16 09:12:33 +0200

    Primer commit

---
+++ src/examen_16m_primo/Examen16M.java
@@ -0,0 +1,20 @@
+package examen_16m_primo;
+
+import java.util.Scanner;
+
+public class Examen16M {
+
+	public static void main(String[] args) {
+		
+		Scanner ent = new Scanner(System.in);
+		int numero=0;
+
+		System.out.println("Buenos días, soy Paola Pérez");
+		System.out.println("Introduce un número para saber si es primo o no:");
+		numero=Integer.parseInt(ent.nextLine());
+		
+		
+
+	}
+
+}
